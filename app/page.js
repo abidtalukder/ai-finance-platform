a3c3410a46dc1029812a101b1fd1c9b00bf753a1
Abid Talukder <atalukder30@stuy.edu>
2025-03-19 15:12:08 -0400

    Created hero section and added to page.js

--- app/page.js
+++ app/page.js
@@ -1,9 +1,8 @@
+import HeroSection from "@/components/hero";
 import {Button} from "@/components/ui/button"
 
 export default function Home() {
   return (
-    <Button className='mt-40'>
-      Get Started
-    </Button>
+    <HeroSection />
   );
 }
