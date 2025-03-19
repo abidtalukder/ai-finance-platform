5a9cc037a99db686ec103a04fa6aa6e45f0c12d9
Abid Talukder <atalukder30@stuy.edu>
2025-03-19 16:03:19 -0400

    Added prisma database schema and created function to add users to database or return existing user

--- components/Header.jsx
+++ components/Header.jsx
@@ -11,8 +11,11 @@ import {
 } from "@clerk/nextjs";
 import { Button } from "@/components/ui/button";
 import { LayoutDashboard, PenBox } from "lucide-react";
+import { checkUser } from "@/lib/checkUser";
 
-const Header = () => {
+const Header = async () => {
+  await checkUser();
+  
   return (
     <div className="fixed top-0 w-full bg-white/80 backdrop-blur-md z-50 border-b">
       <nav className="container mx-auto p-4 flex items-center justify-between">
