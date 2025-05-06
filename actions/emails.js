87ad2f50924696250f41e236051e6257103e3c8b
Abid Talukder <atalukder30@stuy.edu>
2025-05-06 17:42:12 -0400

    changed env variable for resend api

--- actions/emails.js
+++ actions/emails.js
@@ -3,7 +3,7 @@
 import { Resend } from "resend";
 
 export async function sendEmail({ to, subject, react }) {
-  const resend = new Resend(process.env.RESEND_KEY || "");
+  const resend = new Resend(process.env.RESEND_API_KEY || "");
 
   try {
     const data = await resend.emails.send({
